ab2e6408ef8542455e14a9e06da46b98495d19fa
Lorenz Winkler <winlor01@gmail.com>
2020-05-01 20:12:10 +0200

    Now implemented bute-force. This solution is not fast enough though

--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Linq;
 
     type Classes = Elf=0| Human =1|Orc=2|Beast=3|Troll=4|Undead=5|Dragon=6|Goblin=7|Elemental=8|God=9|Dwarf=10|Ogre=11|Naga=12|Demon=13|Aqir=14
     type Traits= Mage=0|Warrior=1|Druid=2|DemonHunter=3|Wizard=4|Shaman=5|Warlock=6|Hunter=7|Assassin=8|Mech=9|Knight=10|Priest=11
@@ -10,37 +11,57 @@ let main argv =
 
     printfn "Hello World from F#!"
     let units=[
-        ("Crystal Maiden", [Classes.Human], [Traits.Mage],1),
-        ("Axe",[Classes.Orc],[Traits.Warrior],1),
-        ("Enchantress",[Classes.Beast],[Traits.Druid],1),
-        ("Tusk",[],[],1),
-        ("Anti-mage",[],[],1),
-        ("Dark Willow",[],[],1),
-        ("Shadow Shaman",[],[],1),
-        ("Witch Doctor",[],[],1),
-        ("Drow Ranger",[],[],1),
-        ("Winter Wyvern",[],[],1),
-        ("Bounty Hunter",[],[],1),
-        ("Clockwork",[],[],1),
-        ("Tinker",[],[],1),
-        ("Tiny",[],[],1),
-        ("Mars",[],[],1),
+        ("Crystal Maiden", [Classes.Human], [Traits.Mage],1);
+        ("Axe",[Classes.Orc],[Traits.Warrior],1);
+        ("Enchantress",[Classes.Beast],[Traits.Druid],1);
+        ("Tusk",[Classes.Beast],[Traits.Warrior],1);
+        ("Anti-mage",[Classes.Elf],[Traits.DemonHunter],1);
+        ("Dark Willow",[Classes.Elf],[Traits.Wizard],1);
+        ("Shadow Shaman",[Classes.Troll],[Traits.Shaman],1);
+        ("Witch Doctor",[Classes.Troll],[Traits.Warlock],1);
+        ("Drow Ranger",[Classes.Undead],[Traits.Hunter],1);
+        ("Winter Wyvern",[Classes.Undead; Classes.Dragon],[Traits.Mage],1);
+        ("Bounty Hunter",[Classes.Goblin],[Traits.Assassin],1);
+        ("Clockwork",[Classes.Goblin],[Traits.Mech],1);
+        ("Tinker",[Classes.Goblin],[Traits.Mech],1);
+        ("Tiny",[Classes.Elemental],[Traits.Warrior],1);
+        ("Mars",[Classes.God],[Traits.Warrior],1);
         
-        ("Beastmaster",[],[],2),
-        ("Juggernaut",[],[],2),
-        ("Bloodseeker",[],[],2),
-        ("Ogre Magi",[],[],2),
-        ("Sniper",[],[],2),
-        ("Furion",[],[],2),
-        ("Mirana",[],[],2),
-        ("Batrider",[],[],2),
-        ("Abbadon",[],[],2),
-        ("Timbersaw",[],[],2),
-        ("Morphling",[],[],2),
-        ("Oracle",[],[],2),
-        ("Slark",[],[],2),
-        ("Chaos Knight",[],[],2)
+        ("Beastmaster",[Classes.Orc],[Traits.Hunter],2);
+        ("Juggernaut",[Classes.Orc],[Traits.Warrior],2);
+        ("Bloodseeker",[Classes.Orc],[Traits.Shaman],2);
+        ("Ogre Magi",[Classes.Ogre],[Traits.Mage],2);
+        ("Sniper",[Classes.Dwarf],[Traits.Hunter],2);
+        ("Furion",[Classes.Elf],[Traits.Druid],2);
+        ("Mirana",[Classes.Elf],[Traits.Hunter],2);
+        ("Batrider",[Classes.Troll],[Traits.Knight],2);
+        ("Abbadon",[Classes.Undead],[Traits.Knight],2);
+        ("Timbersaw",[Classes.Goblin],[Traits.Mech],2);
+        ("Morphling",[Classes.Elemental],[Traits.Warrior],2);
+        ("Oracle",[Classes.God],[Traits.Priest],2);
+        ("Slark",[Classes.Naga],[Traits.Assassin],2);
+        ("Chaos Knight",[Classes.Demon],[Traits.Knight],2)
     ]
 
+    let generateCompositions len=
+        let rec getComposition (comp:((string * Classes list * Traits list *int)list)) (itemstopick:((string * Classes list * Traits list*int)list)) =
+            if comp.Length >= len
+            then [comp]
+            else
+                itemstopick.AsParallel()
+                |>Seq.mapi (fun i item -> item, i )
+                |>Seq.filter (fun (_,index)-> index < itemstopick.Length-(len-comp.Length-1))
+                |>Seq.collect (fun (item, index)-> getComposition (List.append comp [itemstopick.[index]]) (Seq.toList(Seq.skip (index+1) itemstopick)) )
+                |>Seq.toList
 
+        getComposition [] units
+        
+    let comps = generateCompositions 6
+    printfn "%A" comps.Length
+    printfn "%A" (Set.ofList(comps).Count)
+    
+
+    
+    Console.ReadKey()
+    |>ignore
     0 // return an integer exit code
