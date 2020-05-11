6ba16baa216931627150e1cc051fec07df6293bd
lorenzwinkler1 <winlor01@gmail.com>
2020-05-11 19:54:56 +0200

    some changes: Add Genetic Sharp, Add functions for evaluating combos

--- Program.fs
+++ Program.fs
@@ -2,77 +2,16 @@
 
 open System
 open System.Linq;
-
-    type Classes = Elf=0| Human =1|Orc=2|Beast=3|Troll=4|Undead=5|Dragon=6|Goblin=7|Elemental=8|God=9|Dwarf=10|Ogre=11|Naga=12|Demon=13|Aqir=14
-    type Traits= Mage=0|Warrior=1|Druid=2|DemonHunter=3|Wizard=4|Shaman=5|Warlock=6|Hunter=7|Assassin=8|Mech=9|Knight=10|Priest=11
-
-
-    let classRules = dict[
-        Classes.Elf, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Elf classes)|> Seq.length)>=3);
-        Classes.Human, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Human classes)|> Seq.length)>=3);
-        Classes.Orc, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Orc classes)|> Seq.length)>=2);
-        Classes.Beast, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Beast classes)|> Seq.length)>=2);
-        Classes.Troll, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Troll classes)|> Seq.length)>=2);
-        Classes.Undead, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Undead classes)|> Seq.length)>=2);
-        Classes.Dragon, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Dragon classes)|> Seq.length)>=3);
-        Classes.Goblin, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Goblin classes)|> Seq.length)>=3);
-        Classes.Elemental, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Elemental classes)|> Seq.length)>=2);
-        Classes.God, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.God classes)|> Seq.length)>=2);
-        Classes.Dwarf, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Dwarf classes)|> Seq.length)>=1);
-        Classes.Ogre, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Ogre classes)|> Seq.length)>=2);
-        Classes.Naga, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Naga classes)|> Seq.length)>=2);
-
-        Classes.Demon, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            let demoncount=(combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Demon classes)|> Seq.length)
-            let demonhuntercount=(combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.DemonHunter traits)|> Seq.length)
-            demoncount=1 || (demoncount>=1 && demonhuntercount>=2)
-        );        
-        
-        Classes.Aqir, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Classes.Aqir classes)|> Seq.length)>=2);
-    ]
-
-    let traitRules= dict[
-        Traits.Assassin, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.Assassin traits)|> Seq.length)>=3);
-        Traits.DemonHunter, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.DemonHunter traits)|> Seq.length)>=1);
-        Traits.Druid, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.Druid traits)|> Seq.length)>=2);
-        Traits.Hunter, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.Hunter traits)|> Seq.length)>=3);
-        Traits.Knight, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.Knight traits)|> Seq.length)>=2);
-        Traits.Mage, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.Mage traits)|> Seq.length)>=3);
-        Traits.Mech, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.Mech traits)|> Seq.length)>=3);
-        Traits.Priest, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.Priest traits)|> Seq.length)>=1);
-        Traits.Shaman, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.Shaman traits)|> Seq.length)>=2);
-        Traits.Warlock, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.Warlock traits)|> Seq.length)>=2);
-        Traits.Warrior, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.Warrior traits)|> Seq.length)>=3);
-        Traits.Wizard, (fun (combo:(string * Classes list * Traits list *int)list)-> 
-            (combo|>Seq.filter (fun (name, classes, traits, cost) -> Seq.contains Traits.Wizard traits)|> Seq.length)>=2);
-
-    ]
+open GeneticSharp.Domain;
+open DotaAutochessCombosData;
     
+let evaluateSpeciesPerks comp =
+        Enum.GetValues(typeof<Species>):?> (Species [])
+        |> Seq.filter (fun item -> speciesRules.[item] comp)
+
+let evaluateTraitPerks comp =
+        Enum.GetValues(typeof<Traits>):?> (Traits [])
+        |> Seq.filter (fun item -> traitRules.[item] (comp))
 
 let random=Random()
 
@@ -87,48 +26,8 @@ let shuffle seq =
 
     array |> Array.toList
 
-[<EntryPoint>]
-let main argv =
-
-    printfn "Hello World from F#!"
-    let units=[
-        ("Crystal Maiden", [Classes.Human], [Traits.Mage],1);
-        ("Axe",[Classes.Orc],[Traits.Warrior],1);
-        ("Enchantress",[Classes.Beast],[Traits.Druid],1);
-        ("Tusk",[Classes.Beast],[Traits.Warrior],1);
-        ("Anti-mage",[Classes.Elf],[Traits.DemonHunter],1);
-        ("Dark Willow",[Classes.Elf],[Traits.Wizard],1);
-        ("Shadow Shaman",[Classes.Troll],[Traits.Shaman],1);
-        ("Witch Doctor",[Classes.Troll],[Traits.Warlock],1);
-        ("Drow Ranger",[Classes.Undead],[Traits.Hunter],1);
-        ("Winter Wyvern",[Classes.Undead; Classes.Dragon],[Traits.Mage],1);
-        ("Bounty Hunter",[Classes.Goblin],[Traits.Assassin],1);
-        ("Clockwork",[Classes.Goblin],[Traits.Mech],1);
-        ("Tinker",[Classes.Goblin],[Traits.Mech],1);
-        ("Tiny",[Classes.Elemental],[Traits.Warrior],1);
-        ("Mars",[Classes.God],[Traits.Warrior],1);
-        
-        ("Beastmaster",[Classes.Orc],[Traits.Hunter],2);
-        ("Juggernaut",[Classes.Orc],[Traits.Warrior],2);
-        ("Bloodseeker",[Classes.Orc],[Traits.Shaman],2);
-        ("Ogre Magi",[Classes.Ogre],[Traits.Mage],2);
-        ("Sniper",[Classes.Dwarf],[Traits.Hunter],2);
-        ("Furion",[Classes.Elf],[Traits.Druid],2);
-        ("Mirana",[Classes.Elf],[Traits.Hunter],2);
-        ("Batrider",[Classes.Troll],[Traits.Knight],2);
-        ("Abbadon",[Classes.Undead],[Traits.Knight],2);
-        ("Timbersaw",[Classes.Goblin],[Traits.Mech],2);
-        ("Morphling",[Classes.Elemental],[Traits.Warrior],2);
-        ("Oracle",[Classes.God],[Traits.Priest],2);
-        ("Slark",[Classes.Naga],[Traits.Assassin],2);
-        ("Chaos Knight",[Classes.Demon],[Traits.Knight],2);
-
-        ("",[],[],3);
-
-    ]
-
-    let generateRandomComposition len=
-        let rec getComposition (comp:((string * Classes list * Traits list *int)list)) (itemstopick:((string * Classes list * Traits list*int)list)) =
+let generateRandomComposition len=
+        let rec getComposition (comp:((string * Species list * Traits list *int)list)) (itemstopick:((string * Species list * Traits list*int)list)) =
             if comp.Length >= len
             then comp
             else
@@ -136,15 +35,44 @@ let main argv =
                 getComposition (List.append comp [itemstopick.[index]]) (List.skip (index+1) itemstopick)
 
         getComposition [] (shuffle units)
+
+type DotaAutochessChromosome() =
+    inherit GeneticSharp.Domain.Chromosomes.ChromosomeBase(10)
+    override x.CreateNew ()=
+            DotaAutochessChromosome ()
+    override x.GenerateGene geneIndex =
+            GeneticSharp.Domain.Chromosomes.Gene geneIndex
         
 
-    for item in [1..100] do
-        generateRandomComposition 6
-        |> Seq.iter (fun (name, classes, traits, cost) -> printf "%A " name)
-        |> printfn "%A"
-        
+
+
+
+[<EntryPoint>]
+let main argv =
+
+    printfn "Hello World from F#!"
+    
+
+   
         
 
+    for item in [1..50] do
+        let comp=generateRandomComposition 10
+        Console.Write("Units: ")
+        comp
+        |> Seq.iter (fun (name, species, traits, cost) -> printf "%A " name)
+        Console.Write("\nSpecies Perks: ")
+        evaluateSpeciesPerks comp
+        |> Seq.iter (fun speciesPerk -> printf "%A " speciesPerk)
+        Console.Write("\nTrait Perks: ")
+        evaluateTraitPerks comp
+        |> Seq.iter (fun traitPerk -> printf "%A " traitPerk)
+        Console.WriteLine("\n\n")
+        ()
+
+    let ga=GeneticAlgorithm(null,null,null,null,null)
+
+    
     
     Console.ReadKey()
     |>ignore
---
+++ data.fs
@@ -0,0 +1,103 @@
+module DotaAutochessCombosData
+    type Species = Elf=0| Human =1|Orc=2|Beast=3|Troll=4|Undead=5|Dragon=6|Goblin=7|Elemental=8|God=9|Dwarf=10|Ogre=11|Naga=12|Demon=13|Aqir=14
+    type Traits= Mage=0|Warrior=1|Druid=2|DemonHunter=3|Wizard=4|Shaman=5|Warlock=6|Hunter=7|Assassin=8|Mech=9|Knight=10|Priest=11
+
+
+    let speciesRules = dict[
+        Species.Elf, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Elf species)|> Seq.length)>=3);
+        Species.Human, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Human species)|> Seq.length)>=3);
+        Species.Orc, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Orc species)|> Seq.length)>=2);
+        Species.Beast, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Beast species)|> Seq.length)>=2);
+        Species.Troll, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Troll species)|> Seq.length)>=2);
+        Species.Undead, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Undead species)|> Seq.length)>=2);
+        Species.Dragon, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Dragon species)|> Seq.length)>=3);
+        Species.Goblin, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Goblin species)|> Seq.length)>=3);
+        Species.Elemental, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Elemental species)|> Seq.length)>=2);
+        Species.God, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.God species)|> Seq.length)>=2);
+        Species.Dwarf, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Dwarf species)|> Seq.length)>=1);
+        Species.Ogre, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Ogre species)|> Seq.length)>=2);
+        Species.Naga, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Naga species)|> Seq.length)>=2);
+
+        Species.Demon, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            let demoncount=(combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Demon species)|> Seq.length)
+            let demonhuntercount=(combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.DemonHunter traits)|> Seq.length)
+            demoncount=1 || (demoncount>=1 && demonhuntercount>=2)
+        );        
+        
+        Species.Aqir, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Species.Aqir species)|> Seq.length)>=2);
+    ]
+
+    let traitRules= dict[
+        Traits.Assassin, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.Assassin traits)|> Seq.length)>=3);
+        Traits.DemonHunter, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.DemonHunter traits)|> Seq.length)>=1);
+        Traits.Druid, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.Druid traits)|> Seq.length)>=2);
+        Traits.Hunter, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.Hunter traits)|> Seq.length)>=3);
+        Traits.Knight, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.Knight traits)|> Seq.length)>=2);
+        Traits.Mage, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.Mage traits)|> Seq.length)>=3);
+        Traits.Mech, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.Mech traits)|> Seq.length)>=3);
+        Traits.Priest, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.Priest traits)|> Seq.length)>=1);
+        Traits.Shaman, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.Shaman traits)|> Seq.length)>=2);
+        Traits.Warlock, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.Warlock traits)|> Seq.length)>=2);
+        Traits.Warrior, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.Warrior traits)|> Seq.length)>=3);
+        Traits.Wizard, (fun (combo:(string * Species list * Traits list *int)list)-> 
+            (combo|>Seq.filter (fun (name, species, traits, cost) -> Seq.contains Traits.Wizard traits)|> Seq.length)>=2);
+    ]
+
+    let units=[
+        ("Crystal Maiden", [Species.Human], [Traits.Mage],1);
+        ("Axe",[Species.Orc],[Traits.Warrior],1);
+        ("Enchantress",[Species.Beast],[Traits.Druid],1);
+        ("Tusk",[Species.Beast],[Traits.Warrior],1);
+        ("Anti-mage",[Species.Elf],[Traits.DemonHunter],1);
+        ("Dark Willow",[Species.Elf],[Traits.Wizard],1);
+        ("Shadow Shaman",[Species.Troll],[Traits.Shaman],1);
+        ("Witch Doctor",[Species.Troll],[Traits.Warlock],1);
+        ("Drow Ranger",[Species.Undead],[Traits.Hunter],1);
+        ("Winter Wyvern",[Species.Undead; Species.Dragon],[Traits.Mage],1);
+        ("Bounty Hunter",[Species.Goblin],[Traits.Assassin],1);
+        ("Clockwork",[Species.Goblin],[Traits.Mech],1);
+        ("Tinker",[Species.Goblin],[Traits.Mech],1);
+        ("Tiny",[Species.Elemental],[Traits.Warrior],1);
+        ("Mars",[Species.God],[Traits.Warrior],1);
+        
+        ("Beastmaster",[Species.Orc],[Traits.Hunter],2);
+        ("Juggernaut",[Species.Orc],[Traits.Warrior],2);
+        ("Bloodseeker",[Species.Orc],[Traits.Shaman],2);
+        ("Ogre Magi",[Species.Ogre],[Traits.Mage],2);
+        ("Sniper",[Species.Dwarf],[Traits.Hunter],2);
+        ("Furion",[Species.Elf],[Traits.Druid],2);
+        ("Mirana",[Species.Elf],[Traits.Hunter],2);
+        ("Batrider",[Species.Troll],[Traits.Knight],2);
+        ("Abbadon",[Species.Undead],[Traits.Knight],2);
+        ("Timbersaw",[Species.Goblin],[Traits.Mech],2);
+        ("Morphling",[Species.Elemental],[Traits.Warrior],2);
+        ("Oracle",[Species.God],[Traits.Priest],2);
+        ("Slark",[Species.Naga],[Traits.Assassin],2);
+        ("Chaos Knight",[Species.Demon],[Traits.Knight],2);
+
+    ]
